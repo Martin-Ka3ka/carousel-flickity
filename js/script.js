54d06cb0bb35b0d1eae32cd88a1ee9d8cdb7b9f2
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-01-28 23:29:32 +0100

    add new files and modified css

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/*Flickity*/
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    cellAlign: 'center',
+    contain: true,
+    pageDots: false,
+    hash: true,
+});
+
+var flkty = new Flickity('.main-carousel');
+
+var progressBar = document.querySelector('.progress-bar');
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+});
+
+var restart = document.querySelector('.restart');
+
+restart.addEventListener('click', function () {
+    var flkty = new Flickity('.main-carousel');
+    flkty.select(0);
+})
